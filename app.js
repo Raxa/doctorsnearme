e788f4da259b7fc76122c23c1a26f9d907b1f597
Manuri <manuriamaya@hotmail.com>
2014-07-28 01:18:57 +0530

    Commit#82 New app icon test

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ Ext.application({
     models: ['Location', 'Memory', 'Comment', 'OpenmrsLocation'],
 
     icon: {
-        '57': 'resources/icons/Icon.png',
-        '72': 'resources/icons/Icon~ipad.png',
-        '114': 'resources/icons/Icon@2x.png',
-        '144': 'resources/icons/Icon~ipad@2x.png'
+        '57': 'resources/icons/AppIcon.png',
+        '72': 'resources/icons/AppIcon~ipad.png',
+        '114': 'resources/icons/AppIcon@2x.png',
+        '144': 'resources/icons/AppIcon~ipad@2x.png'
     },
 
     isIconPrecomposed: true,
